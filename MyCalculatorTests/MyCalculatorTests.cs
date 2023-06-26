ea480692419d4ee8ccd95aa929eb0735091c616f
Richard Chalk <barmybrit@gmail.com>
2023-06-26 08:08:52 +0200

    Including exercises

--- MyCalculatorTests/MyCalculatorTests.cs
+++ MyCalculatorTests/MyCalculatorTests.cs
@@ -33,5 +33,21 @@ namespace TestCaseNUnitTests
         {
             return _sut.Subtract(a, b);
         }
+
+        [TestCase(2, 3, 4, ExpectedResult = 24)]
+        [TestCase(-2, 3, 7, ExpectedResult = -42)]
+        [TestCase(0, 0, 0, ExpectedResult = 0)]
+        public int Multiply_Test(int a, int b, int c)
+        {
+            return _sut.Multiply (a, b, c);
+        }
+
+        [TestCase(5.5, 2, ExpectedResult = 2.75)]
+        [TestCase(10, 2.5, ExpectedResult = 4)]
+        [TestCase(3, 2, ExpectedResult = 1.5)]
+        public decimal Divide_Test(decimal a, decimal b)
+        {
+            return _sut.Divide(a, b);
+        }
     }
 }
--- TestCaseNUnit/MyCalculator.cs
+++ TestCaseNUnit/MyCalculator.cs
@@ -17,5 +17,21 @@ namespace TestCaseNUnit
         {
             return a - b;
         }
+
+        public int Multiply(int a, int b, int c)
+
+        {
+            return a * b * c;
+        }
+
+        public decimal Divide(decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+        public int Modulo(int a, int b)
+        {
+            return a % b;
+        }
     }
 }
