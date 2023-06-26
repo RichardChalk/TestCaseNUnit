82b8285d85c76870bdba42992bfc0926e55710e9
Richard Chalk <barmybrit@gmail.com>
2023-06-26 06:57:08 +0200

    Add project files.

---
+++ TestCaseNUnit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestCaseNUnit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
